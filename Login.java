cb285f6299e7e4adf1a8333f5299fabca3dbd41e
Sayali2819 <108470808+Sayali2819@users.noreply.github.com>
2024-06-06 14:04:44 +0530

    Update Login.java
    login

--- Login.java
+++ Login.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Login {
+public class Login 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		
