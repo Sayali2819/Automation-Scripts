a195e7173bd98e9c925dcaaf388da509c057587d
Sayali2819 <108470808+Sayali2819@users.noreply.github.com>
2024-06-06 13:00:17 +0530

    Add files via upload

---
+++ CrossBrowserTesting.java
@@ -0,0 +1,51 @@
+package datadriven;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CrossBrowserTesting {
+
+	public static void main(String[] args) {
+		String browser; 
+		WebDriver driver;
+		String username = "standard_user";
+	    String password = "secret_sauce";
+	    
+		System.out.println("Enter the browser name : 1.Chrome  2.FireFox   3.Edge");
+		Scanner sc = new Scanner(System.in);
+		browser = sc.next();
+		
+		if(browser.equalsIgnoreCase("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		else
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		driver.get("https://www.saucedemo.com/v1/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("user-name")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("login-button")).click();
+		driver.close();
+	}
+	
+	
+
+}
