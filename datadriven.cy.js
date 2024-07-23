36cc1fa5f130800d394ae15bdaa5f1d83717cd0c
Sayali2819 <108470808+Sayali2819@users.noreply.github.com>
2024-07-23 17:31:14 +0530

    Update datadriven.cy.js

--- datadriven.cy.js
+++ datadriven.cy.js
@@ -1,35 +1,41 @@
-describe('Data Driven Testing', () => {
+describe('Data Driven Testing', () => 
+{
 
-    it('Test', () => {
-        cy.fixture("orangehrm2").then((data) => {
-
+    it('Test', () => 
+    {
+        cy.fixture("orangehrm2").then((data) => 
+        {
             cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
-
-            data.forEach((userdata) => {
+            
+            data.forEach((userdata) => 
+            {
                 cy.get('input[name="username"]').type(userdata.username);
                 cy.get('input[name="password"]').type(userdata.password);
                 cy.get('button[type="submit"]').click();
 
-                cy.url().then((url) => {
-                    if (url.includes('/dashboard')) {
+                cy.url().then((url) => 
+                {
+                    if (url.includes('/dashboard')) 
+                    {
                       cy.log('Successful login');
                       
                       cy.get('.oxd-userdropdown-tab > .oxd-icon').click();
                       cy.get('a[href="/web/index.php/auth/logout"]').click();
                       cy.wait(2000);
 
-                    } else {
-                //       cy.log('Invalid Credentials');
-                    cy.get('p.oxd-alert-content-text').then(($p) => {
-                        if ($p.is(':visible')) {
+                    } 
+                    else 
+                    {
+                    cy.get('p.oxd-alert-content-text').then(($p) => 
+                    {
+                        if ($p.is(':visible')) 
+                        {
                         const errorMessage = $p.text();
                         cy.log('Error Message: ', errorMessage);
                         } 
 
-                        });
-                    }
-                
+                     });
+                    }                
             });
             cy.wait(1000);
         })
