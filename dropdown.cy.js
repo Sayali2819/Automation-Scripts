c498f898fd297ca05e0b7fceac341b5d29e4d570
Sayali2819 <108470808+Sayali2819@users.noreply.github.com>
2024-07-30 14:30:52 +0530

    Update dropdown.cy.js

--- dropdown.cy.js
+++ dropdown.cy.js
@@ -7,11 +7,7 @@ describe('handle drop down', () => {
 
     it('dropdown without select', () => {
         cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/");
-        
-        // Click on the dropdown to make the search field visible
         cy.get('#select2-billing_country-container').click();
-        
-        // Wait for the search field to be visible and type in the search term
         cy.get('.select2-search__field').type('Iceland').type('{enter}');
         cy.get('#select2-billing_country-container').should('have.text', 'Iceland');
     });
@@ -20,7 +16,6 @@ describe('handle drop down', () => {
         cy.visit('https://www.wikipedia.org/');
         cy.get('#searchInput').type('Delhi');
         cy.get('.suggestion-title').contains('Delhi University').click();
-    })
-
+    }) 
     
 });
